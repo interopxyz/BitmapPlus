d38e62cfa014cd19243455d7c5fe305f7399004b
Mans-TTCore <interop.xyz@gmail.com>
2023-11-11 23:46:50 -0500

    Update Diffusion.cs

--- BitmapPlus/Utilities/Diffusion.cs
+++ BitmapPlus/Utilities/Diffusion.cs
@@ -48,14 +48,12 @@ namespace BitmapPlus
                         A1[x, y] = A0[x, y] + (A0[x0, y0]+ A0[x0, y1]+ A0[x1, y0]+ A0[x1, y1]) * d1 * kc+ (A0[x, y0] + A0[x, y1] + A0[x0, y] + A0[x1, y]) * d1 * ka;
 
                         A1[x, y] += (-1.0) * A0[x, y] * d1;
-
                         A1[x, y] -= A0[x, y] * B0[x, y] * B0[x, y];
                         A1[x, y] += f * (1.0 - A0[x, y]);
 
                         B1[x, y] = B0[x, y] + (B0[x0, y0]+ B0[x0, y1]+ B0[x1, y0] + B0[x1, y1]) * d2 * kc + (B0[x, y0]+ B0[x, y1]+ B0[x0, y] + B0[x1, y]) * d2 * ka;
 
                         B1[x, y] += (-1.0) * B0[x, y] * d2;
-
                         B1[x, y] += A0[x, y] * B0[x, y] * B0[x, y];
                         B1[x, y] -= (f + k) * B0[x, y];
                     }
